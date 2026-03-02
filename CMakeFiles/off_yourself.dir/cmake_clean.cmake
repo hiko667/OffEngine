201a7759881e95da147ea7fe606e2193d10adbf1
hiko667 <91916@student.pb.edu.pl>
2026-03-02 17:01:23 +0100

    initial commit

---
+++ CMakeFiles/off_yourself.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/off_yourself.dir/src/main.cpp.obj"
+  "CMakeFiles/off_yourself.dir/src/main.cpp.obj.d"
+  "liboff_yourself.dll.a"
+  "off_yourself.exe"
+  "off_yourself.exe.manifest"
+  "off_yourself.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/off_yourself.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
